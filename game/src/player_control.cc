7976b77e3606aad8bea1150155345c10b03aa8d8
materfe <mathieurouxdasilva2005@gmail.com>
2025-03-26 12:30:11 +0100

    tried updates but poblems with velocity calculus

--- game/src/player_control.cc
+++ game/src/player_control.cc
@@ -9,17 +9,8 @@ void Car::Tick(const float delta_time) {
     position_.x += velocity_.x / delta_time;
     position_.y += velocity_.y / delta_time;
 
-    velocity_.x -= delta_time /2;
-    velocity_.y -= delta_time /2;
-    if(velocity_.x < 0.0f)
-    {
-      velocity_.x = 0.0;
-    }
-    if(velocity_.y < 0.0f)
-    {
-      velocity_.y = 0.0f;
-    }
+    //velocity_.x -= delta_time /2;
+    //sssvelocity_.y -= delta_time /2;
   }
 }
-
 }
--- main/client.cc
+++ main/client.cc
@@ -11,9 +11,11 @@
 
 int main() {
   static constexpr std::int8_t player_amount = 10;
-  static constexpr sf::Vector2f player_position(600.0f, 350.0f);
+  static constexpr sf::Vector2f player_one_position(400.0f, 350.0f);
+  static constexpr sf::Vector2f player_two_position(800.0f, 350.0f);
   static constexpr float player_radius = 25.0f;
-  static constexpr sf::Color player_color(200, 35, 50, 255);
+  static constexpr sf::Color player_one_color(200, 35, 50, 255);
+  static constexpr sf::Color player_two_color(30, 30, 255, 255);
   static constexpr sf::Color player_outline_color(233, 233, 0, 255);
   static constexpr float player_given_velocity = 30.0f;
   static constexpr float player_speed = 5.0f;
@@ -21,13 +23,20 @@ int main() {
   //manager
   micromachine::car_game_manager::Manager manager(player_amount);
   //player one
-  micromachine::player::Car player_one(player_position);
+  micromachine::player::Car player_one(player_one_position);
+  micromachine::player::Car player_two(player_two_position);
   manager.AddPlayer(player_one);
+  manager.AddPlayer(player_two);
 
-  auto player_representation = sf::CircleShape(player_radius, 3); //making triangles
-  player_representation.setFillColor(player_color);
-  player_representation.setOutlineColor(player_outline_color);
-  player_representation.setOutlineThickness(-2.0f);
+  auto player_representation_01 = sf::CircleShape(player_radius, 3); //making triangles
+  player_representation_01.setFillColor(player_one_color);
+  player_representation_01.setOutlineColor(player_outline_color);
+  player_representation_01.setOutlineThickness(-2.0f);
+
+  auto player_representation_02 = sf::CircleShape(player_radius, 3); //making triangles
+  player_representation_02.setFillColor(player_two_color);
+  player_representation_02.setOutlineColor(player_outline_color);
+  player_representation_02.setOutlineThickness(-2.0f);
 
   sf::RenderWindow window(sf::VideoMode({1280, 720}), "connect_8");
   window.setFramerateLimit(60);
@@ -38,6 +47,10 @@ int main() {
 
   bool isOpen = true;
   sf::Clock deltaClock;
+
+  std::int8_t framerate_limit = 60;
+  window.setFramerateLimit(framerate_limit);
+  window.setVerticalSyncEnabled(true);
 
   while (isOpen) {
 
@@ -50,28 +63,33 @@ int main() {
       }
       if (event->is<sf::Event::KeyPressed>()) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-          sf::Vector2<float> new_velocity = sf::Vector2f(0.0f, -1 * player_given_velocity * player_speed);
+          sf::Vector2<float> new_velocity_01 = sf::Vector2f(0.0f, -1 * (player_given_velocity * player_speed));
+          sf::Vector2<float> new_velocity_02 = sf::Vector2f(0.0f, (player_given_velocity * player_speed));
 
           //player_one.MoveAt(new_velocity);
-          manager.AllPlayers()[0].MoveAt(new_velocity);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-          sf::Vector2<float> new_velocity = sf::Vector2f(0.0f, player_given_velocity * player_speed);
+          manager.AllPlayers()[0].MoveAt(new_velocity_01);
+          manager.AllPlayers()[1].MoveAt(new_velocity_02);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+          sf::Vector2<float> new_velocity_01 = sf::Vector2f(0.0f, player_given_velocity * player_speed);
+          sf::Vector2<float> new_velocity_02 = sf::Vector2f(0.0f, -1 * (player_given_velocity * player_speed));
 
           //player_one.MoveAt(new_velocity);
-          manager.AllPlayers()[0].MoveAt(new_velocity);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-          sf::Vector2<float> new_velocity = sf::Vector2f(-1 * player_given_velocity * player_speed, 0.0f);
+          manager.AllPlayers()[0].MoveAt(new_velocity_01);
+          manager.AllPlayers()[1].MoveAt(new_velocity_02);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+          sf::Vector2<float> new_velocity_01 = sf::Vector2f(-1 * player_given_velocity * player_speed, 0.0f);
+          sf::Vector2<float> new_velocity_02 = sf::Vector2f(player_given_velocity * player_speed, 0.0f);
 
           //player_one.MoveAt(new_velocity);
-          manager.AllPlayers()[0].MoveAt(new_velocity);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-          sf::Vector2<float> new_velocity = sf::Vector2f(player_given_velocity * player_speed, 0.0f);
+          manager.AllPlayers()[0].MoveAt(new_velocity_01);
+          manager.AllPlayers()[1].MoveAt(new_velocity_02);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+          sf::Vector2<float> new_velocity_01 = sf::Vector2f(player_given_velocity * player_speed, 0.0f);
+          sf::Vector2<float> new_velocity_02 = sf::Vector2f(-1 * (player_given_velocity * player_speed), 0.0f);
 
           //player_one.MoveAt(new_velocity);
-          manager.AllPlayers()[0].MoveAt(new_velocity);
+          manager.AllPlayers()[0].MoveAt(new_velocity_01);
+          manager.AllPlayers()[1].MoveAt(new_velocity_02);
         }
       }
     }
@@ -89,12 +107,14 @@ int main() {
     //Ticks --------------------------------------------------------------------------------------------------------
     manager.AllTicks(static_cast<float>(delta));
 
-    player_representation.setPosition(manager.AllPlayers()[0].Position());
+    player_representation_01.setPosition(manager.AllPlayers()[0].Position());
+    player_representation_02.setPosition(manager.AllPlayers()[1].Position());
 
 
     //DRAW ---------------------------------------------------------------------------------------------------------
     window.clear();
-    window.draw(player_representation);
+    window.draw(player_representation_01);
+    window.draw(player_representation_02);
     window.display();
   }
 
